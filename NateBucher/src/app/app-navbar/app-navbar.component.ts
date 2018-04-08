47b0d33d5cd4feb4c3b9a507f825bd57e76ff415
Nate Bucher <n8bizall@yahoo.com>
2018-04-08 18:47:55 -0400

    angular menu

---
+++ NateBucher/src/app/app-navbar/app-navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-navbar',
+  templateUrl: './app-navbar.component.html',
+  styles: []
+})
+export class AppNavbarComponent implements OnInit {
+
+  constructor() { 
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
